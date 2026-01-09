d4d6d3f650dc744f6f37952dfa1fddee0accfc31
Subhaji-Dutta <146566101+Subhaji-Dutta@users.noreply.github.com>
2026-01-09 14:40:55 +0530

    Fix fonts and add Clerk for Next 14

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,51 +1,46 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "sonner";
 import TanStackProvider from "@/components/providers/TanStackProvider";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const inter = Inter({
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  variable: "--font-inter",
 });
 
 export const metadata: Metadata = {
   title: "Dentiva - AI Powered Dental Assistant",
   description:
-    "Get instant dental advice through voice calls with our AI assistant. Avaiable 24/7.",
+    "Get instant dental advice through voice calls with our AI assistant. Available 24/7.",
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
-    <TanStackProvider>
-      <ClerkProvider
-        appearance={{
-          variables: {
-            colorPrimary: "#e78a53",
-            colorBackground: "#f3f4f6",
-            colorText: "#111827",
-            colorTextSecondary: "#6b7280",
-            colorInputBackground: "#f3f4f6",
-          },
-        }}
-      >
-        <html lang="en">
-          <body className={`${geistSans.variable} ${geistMono.variable} antialiased dark`}>
+    <html lang="en">
+      <body className={`${inter.variable} antialiased dark`}>
+        <TanStackProvider>
+          <ClerkProvider
+            appearance={{
+              variables: {
+                colorPrimary: "#e78a53",
+                colorBackground: "#f3f4f6",
+                colorText: "#111827",
+                colorTextSecondary: "#6b7280",
+                colorInputBackground: "#f3f4f6",
+              },
+            }}
+          >
             <Toaster />
             {children}
-          </body>
-        </html>
-      </ClerkProvider>
-      </TanStackProvider>
+          </ClerkProvider>
+        </TanStackProvider>
+      </body>
+    </html>
   );
 }
